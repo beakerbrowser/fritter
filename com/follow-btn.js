0ef267a37459059fee94d23206546d844fa4b179
Tara Vancil <tbvanc@gmail.com>
2018-01-02 21:02:09 -0600

    Add an unfollow hover state to the follow button

--- com/follow-btn.js
+++ com/follow-btn.js
@@ -2,20 +2,50 @@
 
 const yo = require('yo-yo')
 
+// globals
+let isHovering = false
+let btnText
+
 // exported api
 // =
 
 module.exports = function renderFollowButton (profile) {
   if (profile.isCurrentUser) return ''
-  var cls = app.isCurrentUserFollowing(profile) ? 'following' : ''
+
+  if (app.isCurrentUserFollowing(profile)) {
+    if (isHovering) {
+      btnText = 'Unfollow'
+      cls = 'unfollow'
+    } else {
+      btnText = 'Following'
+      cls = 'following'
+    }
+  } else {
+    btnText = 'Follow'
+    cls = ''
+  }
   return yo`
-    <button class="follow-btn btn ${cls}" onclick=${onToggleFollowing}>
-      ${app.isCurrentUserFollowing(profile) ? 'Following' : 'Follow'}
+    <button class="follow-btn btn ${cls}" onclick=${onToggleFollowing} onmouseover=${onMouseOver} onmouseout=${onMouseOut}>
+      ${btnText}
     </button>`
 
   async function onToggleFollowing (e) {
     e.preventDefault()
     e.stopPropagation()
     await app.toggleFollowing(profile)
+  }
+
+  function onMouseOver () {
+    if (!isHovering) {
+      isHovering = true
+      app.render()
+    }
+  }
+
+  function onMouseOut () {
+    if (isHovering) {
+      isHovering = false
+      app.render()
+    }
   }
 }
