61fe779b71618fec7cea5c1e5889cec4ebfeb092
Tara Vancil <tbvanc@gmail.com>
2018-01-08 12:57:20 -0600

    Re-order the create profile form and profile picker

--- views/new-user.js
+++ views/new-user.js
@@ -20,23 +20,14 @@ module.exports = function () {
           <a href="https://datproject.org">Dat</a>, and
           <a href="https://github.com/beakerbrowser/webdb">WebDB</a>.
         </p>
-
-        <h2>Fritter profiles</h2>
-        <p>
-          A Fritter profile is a bit different than a Twitter or Facebook profile.
-          It's simply a website! This means you can look at all the files that
-          comprise a profile. Check out
-          <a href="dat://232ac2ce8ad4ed80bd1b6de4cbea7d7b0cad1441fa62312c57a6088394717e41/" target="_blank">
-          Dog Legs McBoots's profile</a> to see for yourself.
-        </p>
-      </div>
-
-      <div>
-        ${renderProfilePicker()}
       </div>
 
       <div>
         ${renderProfileEditor()}
+      </div>
+
+      <div>
+        ${renderProfilePicker()}
       </div>
 
       <div>
