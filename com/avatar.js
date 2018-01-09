67e735ed8efde83122c7dac71c0849125ec72064
Tara Vancil <tbvanc@gmail.com>
2018-01-08 18:42:54 -0600

    Support passing in a profile URL to renderAvatar

--- com/avatar.js
+++ com/avatar.js
@@ -5,6 +5,7 @@ const imgWithFallbacks = require('./img-with-fallbacks')
 
 module.exports = function renderAvatar (profile, cls = '') {
   if (!profile) return ''
+  if (!profile.url && !profile.getRecordOrigin) profile = {url: profile}
   return yo`
     <a href=${app.profileUrl(profile)} onclick=${e => app.gotoProfile(profile, e)} class="avatar-container ${cls}">
       ${imgWithFallbacks(`${profile.getRecordOrigin ? profile.getRecordOrigin() : profile.url}/avatar`, ['png', 'jpg', 'jpeg', 'gif'], {cls: `avatar ${cls}`})}
