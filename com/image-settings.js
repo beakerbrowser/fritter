c90c18598d736df9ec7fa64ce61c9dda596af8c2
Sander Moolin <sandermoolin@gmail.com>
2018-01-17 09:16:21 -0500

    Saved setting - still need to implement, add gif option

--- com/image-settings.js
+++ com/image-settings.js
@@ -16,18 +16,18 @@ module.exports = function renderImageSettings(){
 
       <p>Embedding images creates a more seamless timeline, but can be used to pixel-track users (<a href="https://github.com/beakerbrowser/fritter/issues/10">link</a>). Choose the setting that works best for you:</p>
 
-      <form class="image-settings">
+      <form class="image-settings" onsubmit=${onUpdateImageSettings}>
 
-        <input type="radio" id="choice-all" name="embedImages" value="all">
+        <input ${isCurrentSetting('all') ? 'checked' : ''} type="radio" id="choice-all" name="embedImages" value="all">
         <label for="choice-all">Embed all images</label><br>
 
-        <input type="radio" id="choice-dat" name="embedImages" value="dat">
+        <input ${isCurrentSetting('dat') ? 'checked' : ''} type="radio" id="choice-dat" name="embedImages" value="dat">
         <label for="choice-dat">Embed only images from dat:// sources</label><br>
 
-        <input type="radio" id="choice-dat-followed" name="embedImages" value="dat-followed">
+        <input ${isCurrentSetting('dat-followed') ? 'checked' : ''} type="radio" id="choice-dat-followed" name="embedImages" value="dat-followed">
         <label for="choice-dat-followed">Embed only images from dat:// sources I follow</label><br>
 
-        <input type="radio" id="choice-none" name="embedImages" value="none">
+        <input ${isCurrentSetting('none') ? 'checked' : ''} type="radio" id="choice-none" name="embedImages" value="none">
         <label for="choice-none">Don't embed images</label><br>
 
         <div class="actions">
@@ -37,4 +37,24 @@ module.exports = function renderImageSettings(){
       </form>
     </div>
   `
+
+  async function onUpdateImageSettings(e){
+    e.preventDefault()
+
+    // Load existing settings
+    const settings = JSON.parse(window.localStorage.settings || '{}')
+
+    // Save new value of settings
+    const imageSetting = document.querySelector('input[name=embedImages]:checked').value
+    settings.imageEmbed = imageSetting
+
+    await app.updateSettings(settings)
+
+    app.gotoFeed()
+  }
+
+  function isCurrentSetting(val){
+    const settings = JSON.parse(window.localStorage.settings || '{}')
+    return val === settings.imageEmbed || (!settings.imageEmbed && val === 'none')
+  }
 }
